a98ad76a997b54c2fa38137395a618cbe437cd5b
Eliana Barrios <elianabarrios9827@gmail.com>
2021-06-15 00:26:07 -0300

    creacion-escena-puntajes

--- scenes/characters/princess/peluda/PrincessPeluda.gd
+++ scenes/characters/princess/peluda/PrincessPeluda.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var movement = Vector2(500,0)
-
+var levelStart = false
 var isOnAir = false
 
 const VECTOR_NORMAL = Vector2(0, -1)
@@ -9,12 +9,18 @@ const VECTOR_NORMAL = Vector2(0, -1)
 func _ready():
 	pass 
 func _physics_process(delta):
-	movement.y += 30
-	if is_on_floor():
-		movement.y = 0
-		isOnAir = false
-	if Input.is_action_just_pressed("ui_up") and isOnAir == false:
-		movement.y = -1100
-		isOnAir = true
-	move_and_slide(movement, VECTOR_NORMAL)
+	if levelStart == true:
+		movement.y += 30
+		if is_on_floor():
+			movement.y = 0
+			isOnAir = false
+		if Input.is_action_just_pressed("touch") and isOnAir == false:
+			movement.y = -1100
+			isOnAir = true
+		move_and_slide(movement, VECTOR_NORMAL)
+	pass
+
+
+func _on_Timer_timeout():
+	levelStart = true
 	pass
