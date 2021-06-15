9de9ea45f45789fdeb0f80b235614893533ee040
MiloVarok <80860312+MiloVarok@users.noreply.github.com>
2021-06-15 00:59:30 -0300

    presentation-scene-more-assets
    Escena de presentacion del desafio, carga de assets

--- scenes/player_select/PlayerSelect.gd
+++ scenes/player_select/PlayerSelect.gd
@@ -7,5 +7,5 @@ func _ready():
 
 
 func _on_Peluda_button_down():
-	get_tree().change_scene("res://scenes/level_1/Level1.tscn")
+	get_tree().change_scene("res://scenes/level_1/Challenge1Presentation.tscn")
 	pass
